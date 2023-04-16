dc41ffbdf0bec053ca7aa7e42489f22f6d5e7350
Erdiwo <erdie2anjaratiana1998@gmail.com>
2023-04-16 11:05:58 +0300

    Mise en place de Encore

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or subdirectory deploy
+    //.setManifestKeyPrefix('build/')
+
+    .addEntry('app', './assets/app.js')
+    .addEntry('hydrology', './assets/js/hydrology.js')
+    .addEntry('bootstrap', ['jquery', 'bootstrap'])
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+    // enables and configure @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
+    })
+    // enables Sass/SCSS support
+    .enableSassLoader()
+    .autoProvidejQuery()
+    .addStyleEntry('css/fontawesome', './node_modules/@fortawesome/fontawesome-free/css/all.css')
+    .addStyleEntry('login', './assets/styles/login.scss')
+;
+
+module.exports = Encore.getWebpackConfig();
